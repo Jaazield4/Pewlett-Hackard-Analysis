06229cce9149e91dcf098bd429b554711bdce5bd
Jaazield4 <85451089+Jaazield4@users.noreply.github.com>
2021-08-05 13:10:57 -0500

    Add files via upload

--- schemal.sql
+++ schemal.sql
@@ -1,4 +1,3 @@
-
 -- Creating tables for PH-EmployeeDB
 Create Table departments (
 	dept_no VARCHAR(4) NOT NULL,
@@ -55,9 +54,42 @@ Create Table titles (
 	from_date Date Not Null,
 	to_date Date Not Null,
 	Foreign Key (emp_no) References salaries (emp_no),
-	Foreign Key (emp_no) References employees (emp_no),
-	Primary Key (emp_no),
-	Unique (emp_no)
+	Foreign Key (emp_no) References employees (emp_no)
 );
 
-Select * From departments;
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Creating a new table with retirement info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
